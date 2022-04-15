0dc3ab79ee84b44fc16250c5a5b8484af1cb97af
504 <504@DESKTOP-M91NLNL>
2022-04-15 17:17:16 +0900

    1

--- ERIYA/src/controller/home/Order.java
+++ ERIYA/src/controller/home/Order.java
@@ -2,12 +2,20 @@ package controller.home;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import controller.Main;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 
 public class Order implements Initializable{
+
+	 @FXML
+	   private BorderPane borderpane;
 
     @FXML
     private Label lblback;
@@ -15,12 +23,23 @@ public class Order implements Initializable{
     @FXML
     void back(MouseEvent event) {
     	System.out.println("뒤로가기 버튼을 눌렀습니다.");
-		Home.home.loadpage("/view/home/home.fxml");
+		Main.instance.loadpage("/view/home/home.fxml");
     }
     
+    public void loadpage(String page) {
+		try {
+			Parent parent = FXMLLoader.load(getClass().getResource(page));
+			borderpane.setCenter(parent);
+		} catch (Exception e) {
+			System.out.println("Load 오류 : " + e);
+		}
+	}
 @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
 
     }
 }
+
+
+
 
